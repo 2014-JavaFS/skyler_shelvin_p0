b51b3de8da5062fb1940ed5f5d8161fc3308a79d
Skyler_Shelvin <skylershelvin@gmail.com>
2024-07-03 17:23:40 -0500

    created Account model, service , and controller

---
+++ src/Account/Account.java
@@ -0,0 +1,39 @@
+package Account;
+
+public class Account {
+
+    int accountId;
+
+    String email;
+
+    String password;
+
+    public Account(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
